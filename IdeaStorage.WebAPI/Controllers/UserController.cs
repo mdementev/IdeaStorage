6fe8053ca6d84751d6684fb528f10d6ef7c2c9ee
Alexey Kryukov <alexey.kryukov.91@gmail.com>
2014-11-28 11:47:06 +0300

    Added User controller

---
+++ IdeaStorage.WebAPI/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿namespace IdeaStorage.WebAPI.Controllers
+{
+    using System.Net;
+    using System.Net.Http;
+    using System.Security.Principal;
+    using System.Threading;
+    using System.Web.Http;
+
+    using BusinessLogic.EntityManagers;
+
+    using IdeaStorage.EntriesModel.Entries;
+
+    /// <summary>
+    /// Contains logic for working with users in Idea Storage API.
+    /// </summary>
+    [RoutePrefix("user")]
+    public class UserController : ApiController
+    {
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// Gets the logged in user.
+        /// </summary>
+        /// <param name="request">The request.</param>
+        /// <returns>The instance of <see cref="User"/></returns>
+        [HttpPost]
+        [Route("GetLoggedInUser")]
+        public HttpResponseMessage GetLoggedInUser(HttpRequestMessage request)
+        {
+            IIdentity identity = Thread.CurrentPrincipal.Identity;
+            IUserManager manager = new UserManager();
+            User loggedInUser = manager.FindUserByEmail(identity.Name);
+
+            return request.CreateResponse(HttpStatusCode.OK, loggedInUser);
+        }
+
+        #endregion
+    }
+}
