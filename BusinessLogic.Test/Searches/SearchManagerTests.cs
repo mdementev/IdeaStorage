d0c3d4af795ca6cfeb3579db1131ffc4d986deea
Michael <none@none>
2014-11-28 19:02:47 +0300

    Added DuoVia fuzzy library. Added SearchNodesByTags

--- BusinessLogic.Test/Searches/SearchManagerTests.cs
+++ BusinessLogic.Test/Searches/SearchManagerTests.cs
@@ -17,7 +17,7 @@ namespace BusinessLogic.Test.Searches
     public class SearchManagerTests : DatabaseTestFixture
     {
         [Test]
-        public void SearchNodes_ValidTerms_ResultsFound()
+        public void SearchNodes_SearchByValue_ResultsFound()
         {
             User user = new User
             {
@@ -60,7 +60,69 @@ namespace BusinessLogic.Test.Searches
 
 
             SearchManager searchManager = new SearchManager();
-            var s2 = searchManager.FindNodesByString("text2 bad", true);
+            var s2 = searchManager.FindNodesByValue("text2 bad", true);
+        }
+
+
+        [Test]
+        public void SearchNodes_SearchByTags_ResultsFound()
+        {
+            User user = new User
+            {
+                Email = "user@mail.com",
+                FirstName = "First",
+                SecondName = "Second"
+            };
+
+            const string Password = "qwerty";
+
+            IUserManager userManager = new UserManager();
+            int userId = userManager.CreateUser(user, Password);
+
+            Node node = new Node
+            {
+                Created = DateTime.Now,
+                IsDeleted = false,
+                Modified = DateTime.Now,
+                OwnerId = userId,
+                Text = "text12",
+                Title = "title",
+                Tags = new List<Tag> { new Tag { Name = "Simple" } }
+            };
+            NodeManager s1 = new NodeManager();
+
+            s1.CreateNode(node);
+
+            Node node2 = new Node
+            {
+                Created = DateTime.Now,
+                IsDeleted = false,
+                Modified = DateTime.Now,
+                OwnerId = userId,
+                Text = "text2",
+                Title = "title2",
+                Tags = new List<Tag> { new Tag { Name = "Bad" }, new Tag { Name = "Good" } }
+            };
+
+            s1.CreateNode(node2);
+
+
+            Node node3 = new Node
+            {
+                Created = DateTime.Now,
+                IsDeleted = false,
+                Modified = DateTime.Now,
+                OwnerId = userId,
+                Text = "text2",
+                Title = "title2",
+                Tags = new List<Tag> { new Tag { Name = "Bad" }, new Tag { Name = "Good1" } }
+            };
+
+            s1.CreateNode(node3);
+
+
+            SearchManager searchManager = new SearchManager();
+            var s2 = searchManager.FindNodesByTags("simple, bad", true);
 
         }
     }
--- BusinessLogic/Searches/SearchManager.cs
+++ BusinessLogic/Searches/SearchManager.cs
@@ -1,5 +1,6 @@
 ﻿namespace BusinessLogic.Searches
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
 
@@ -11,7 +12,7 @@
 
     public sealed class SearchManager
     {
-        public List<Node> FindNodesByString(string searchTerm, bool caseSensitive)
+        public List<Node> FindNodesByValue(string searchTerm, bool caseSensitive = false)
         {
             List<string> searchTermList = searchTerm.Split().ToList();
             List<NODE> dbNodesList = new List<NODE>();
@@ -36,7 +37,45 @@
 
                 return result;
             }
+        }
+
+        public List<Node> FindNodesByTags(string searchTerm, bool caseSensitive = false)
+        {
+            List<string> searchTermList = searchTerm.Split(',').Select(a=> a.Trim()).ToList();
+            
+            List<TAG> searchTagsList = new List<TAG>();
+            List<NODE> dbNodesList = new List<NODE>();
+
+            using (IdeaStorageEntities context = new IdeaStorageEntities())
+            {
+
+                foreach (string tagString in searchTermList)
+                {
+                    searchTagsList.AddRange(context.TAGS.Where(t => t.Name.ToLower().Equals(tagString.ToLower())).ToList());
+                }
+
+                List<TAGSET> dbTagSets = new List<TAGSET>();
+                foreach (TAG tag in searchTagsList)
+                {
+                    dbTagSets.AddRange(context.TAGSETS.Where(ts => ts.TagId == tag.TagId).ToList());
+                }
+
+                foreach (TAGSET dbTagSet in dbTagSets)
+                {
+                    dbNodesList.Add(context.NODES.Single(n => n.NodeId == dbTagSet.NodeId));
+                }
+
+                List<NODE> dbResult = (dbNodesList.GroupBy(n => n, n => n.NodeId)
+                    .Select(g => new { g, count = g.Count() })
+                    .OrderByDescending(@t => @t.count)
+                    .Select(@t => @t.g.Key)).ToList();
+
+                List<Node> result = dbResult.Select(n => n.ToModel()).ToList();
+
+                return result;
+            }
 
         } 
+
     }
 }
